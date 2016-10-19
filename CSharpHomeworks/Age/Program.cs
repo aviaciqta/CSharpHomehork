558bbba6ac9a555ee95a8b2014b3818b843b4cd4
aviaciqta <aviaciqta@gmail.com>
2016-10-19 15:30:14 +0300

    Add files via upload

---
+++ CSharpHomeworks/Age/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime Birthday = DateTime.Parse(Console.ReadLine());
+            DateTime today = DateTime.Now;
+            string s = Birthday.ToString("MM.DD.YYYY");
+            int age = today.Year - Birthday.Year;
+            Console.WriteLine(age);
+            Console.WriteLine(age + 10);
+        }
+    }
+}
